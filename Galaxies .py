c6762e9559d772652a77da7f121ea8e5e9cc91ad
Maliha Islam Shefa <167281685+Maliha1983@users.noreply.github.com>
2024-04-30 21:25:14 +0600

    Galaxies

--- Galaxies .py
+++ Galaxies .py
@@ -1,45 +1,4 @@
-"""# importing turtle library
-import turtle
- 
- 
-# creating turtle object
-t = turtle.Turtle()
- 
-# to activate turtle graphics screen
-w = turtle.Screen()
- 
-# speed of turtle's pen
-t.speed(0)
- 
-# creating star
-for i in range(0, 5):
-    t.fd(200)
-    t.right(144)
- 
-# after clicking turtle graphics screen
-# will be terminated
-w.exitonclick()
 
-# importing turtle library
-import turtle
- 
- 
-# creating turtle object
-t = turtle.Turtle()
- 
-# to activate turtle graphics Screen
-w = turtle.Screen()
- 
-# speed of turtle's pen
-t.speed()
- 
-# Creating circle
-t.circle(100)
- 
-# after clicking turtle graphics screen 
-# will be terminated
-w.exitonclick()
-"""
 # importing libraries
 import turtle
 import random
